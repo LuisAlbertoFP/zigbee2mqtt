8ea6eb618e52676c514c3bb7e9fe07c4c94acdf9
Luis Alberto <luisalb@edu.xunta.es>
2026-04-03 12:27:11 +0200

    entrypoint

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -10,27 +10,38 @@ APP_DIR="/app"
 
 APP_SRC_REL="${APP_SRC_REL:-web/app.py}"
 HTML_SRC_REL="${HTML_SRC_REL:-web/templates/index.html}"
+STATIC_SRC_REL="${STATIC_SRC_REL:-web/static}"
 
 log() {
   echo "[entrypoint] $*"
 }
 
 copy_from_source() {
-  mkdir -p "$APP_DIR/templates"
+  mkdir -p "$APP_DIR/templates" "$APP_DIR/static"
   cp "$SRC_DIR/$APP_SRC_REL" "$APP_DIR/app.py"
   cp "$SRC_DIR/$HTML_SRC_REL" "$APP_DIR/templates/index.html"
+  cp -r "$SRC_DIR/$STATIC_SRC_REL/." "$APP_DIR/static/"
 }
 
 copy_from_cache() {
-  mkdir -p "$APP_DIR/templates"
+  mkdir -p "$APP_DIR/templates" "$APP_DIR/static"
   cp "$CACHE_DIR/app.py" "$APP_DIR/app.py"
   cp "$CACHE_DIR/index.html" "$APP_DIR/templates/index.html"
+  if [ -d "$CACHE_DIR/static" ]; then
+    cp -r "$CACHE_DIR/static/." "$APP_DIR/static/"
+  fi
 }
 
 save_cache() {
   mkdir -p "$CACHE_DIR"
   cp "$APP_DIR/app.py" "$CACHE_DIR/app.py"
   cp "$APP_DIR/templates/index.html" "$CACHE_DIR/index.html"
+
+  rm -rf "$CACHE_DIR/static"
+  mkdir -p "$CACHE_DIR/static"
+  if [ -d "$APP_DIR/static" ]; then
+    cp -r "$APP_DIR/static/." "$CACHE_DIR/static/"
+  fi
 }
 
 have_cache() {
@@ -51,6 +62,7 @@ update_from_git() {
 
   [ -f "$SRC_DIR/$APP_SRC_REL" ]
   [ -f "$SRC_DIR/$HTML_SRC_REL" ]
+  [ -d "$SRC_DIR/$STATIC_SRC_REL" ]
 }
 
 log "Iniciando actualización de código"
